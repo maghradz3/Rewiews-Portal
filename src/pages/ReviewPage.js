67c010429809122c57817d6fc8b1d9d71a0b6a70
Levan Maghradze <120563893+maghradz3@users.noreply.github.com>
2023-09-22 14:39:55 +0400

    styling

--- src/pages/ReviewPage.js
+++ src/pages/ReviewPage.js
@@ -3,7 +3,7 @@ import { AddReviewForm } from "../components/Reviews";
 
 export const ReviewPage = () => {
   return (
-    <div>
+    <div className="h-[100vh]">
       <AddReviewForm />
     </div>
   );
